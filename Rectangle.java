6b290efc9cc3d85c32294f39d53e8eee9f030926
Aniket Gajare <66246623+aniketgajare19@users.noreply.github.com>
2020-06-02 18:04:13 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+package Shapes;
+
+public class Rectangle {
+    double length , width;
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    public double getArea(){
+        return (length*width);
+    }
+    public double getPerimeter(){
+        return (2*(length + width));
+    }
+}
+
